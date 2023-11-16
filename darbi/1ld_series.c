451b73bcb0a27c4ce6effe17bad005a98094c14d
ReinisVigulis <sinier2004@gmail.com>
2023-11-16 19:29:43 +0200

    darbi/1ld_series.c

--- darbi/1ld_series.c
+++ darbi/1ld_series.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-double mans_sinuss(double x ) {
-    double a, S;
+long double mans_sinuss(long double x ) {
+    long double a, S;
     int k = 0;
 
     a = pow(-1, k) * pow(x, 4 * k + 2) / 1.0;
     S = a;
-    printf("%.2f\t%8.2f\t%8.2f\n", x, a, S);
+    printf("%.2Le\t%8.2Le\t%8.2Le\n", x, a, S);
 
     while (k < 500) {
         k++;
@@ -20,12 +20,12 @@ double mans_sinuss(double x ) {
 }
 
 int main() {
-    double x = 3, y, yy;
+    long double x = 8.9, y, yy;
     y = sin(x * x);
-    printf("standarta funkcijas sin - y=sin(%.2f * %.2f)= %.2f\n", x, x, y);
+    printf("standarta funkcijas sin - y=sin(%.2Le * %.2Le)= %.2Le\n", x, x, y);
 
     yy = mans_sinuss(x);
-    printf("lietotāja funkcijas sin - y=mana_funkcija(%.2f)= %.2f\n", x, yy);
+    printf("lietotāja funkcijas sin - y=mana_funkcija(%.2Le)= %.2Lf\n", x, yy);
 
     return 0;
 }
